10ba74ff0591922691e36a293be19f7be598d0d7
jdberrocal1 <jdanielb1992@gmail.com>
2018-05-31 09:32:19 -0600

    adding webpack config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import App from './components/App.jsx'
+
+import './styles/style.scss'
 
 ReactDOM.render(
     <App/>,
